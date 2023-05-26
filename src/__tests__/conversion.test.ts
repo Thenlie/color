9c97d03c9ed5fa094f3d56303f922dd32edef293
Leithen Crider <lcrider@cvent.com>
2023-05-26 08:07:59 -0600

    WIP - Improve conversion unit tests

--- src/__tests__/conversion.test.ts
+++ src/__tests__/conversion.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, test } from '@jest/globals';
 import { hexToDec, hexToRgba, decToHex, rgbToHex, rgbToRgba, unknownToRgba, rgbaToArray } from '../conversion';
 
-describe('Conversion module', () => {
+describe('Conversion module (passing ✅)', () => {
   test('hexToDec correctly converts hexadecimal to decimal', () => {
     expect(hexToDec('00')).toEqual(0);
     expect(hexToDec('ff')).toEqual(255);
@@ -10,19 +10,34 @@ describe('Conversion module', () => {
   test('hexToRgba correctly converts hexadecimal to RGBA', () => {
     expect(hexToRgba('#000000')).toEqual('rgba(0,0,0,0)');
     expect(hexToRgba('#ffffff')).toEqual('rgba(255,255,255,0)');
+    expect(hexToRgba('#aabbcc')).toEqual('rgba(170,187,204,0)');
+    expect(hexToRgba('#003400')).toEqual('rgba(0,52,0,0)');
+    expect(hexToRgba('#2aff00')).toEqual('rgba(42,255,0,0)');
+    expect(hexToRgba('#ffffffff')).toEqual('rgba(255,255,255,255)');
+    expect(hexToRgba('#000000ff')).toEqual('rgba(0,0,0,255)');
   });
   test('decToHex correctly converts decimal to hexadecimal', () => {
     expect(decToHex(0)).toEqual('00');
     expect(decToHex(255)).toEqual('ff');
     expect(decToHex(60)).toEqual('3c');
+    expect(decToHex(42)).toEqual('2a');
+    expect(decToHex(52)).toEqual('34');
   });
   test('rgbToHex correctly converts RGB to hexadecimal', () => {
     expect(rgbToHex('rgb(0,0,0)')).toEqual('#00000000');
     expect(rgbToHex('rgb(255,255,255)')).toEqual('#ffffff00');
+    expect(rgbToHex('rgb(24,134,0)')).toEqual('#18860000');
+    expect(rgbToHex('rgb(143,214,12)')).toEqual('#8fd60c00');
+    expect(rgbToHex('rgb(12,6,123)')).toEqual('#0c067b00');
+    expect(rgbToHex('rgb(0,255,123)')).toEqual('#00ff7b00');
   });
   test('rgbToRgba correctly converts RGB to RGBA', () => {
     expect(rgbToRgba('rgb(0,0,0)')).toEqual('rgba(0,0,0,0)');
     expect(rgbToRgba('rgb(255,255,255)')).toEqual('rgba(255,255,255,0)');
+    expect(rgbToRgba('rgb(13,156,24)')).toEqual('rgba(13,156,24,0)');
+    expect(rgbToRgba('rgb(56,67,3)')).toEqual('rgba(56,67,3,0)');
+    expect(rgbToRgba('rgb(123,255,0)')).toEqual('rgba(123,255,0,0)');
+    expect(rgbToRgba('rgb(7,255,245)')).toEqual('rgba(7,255,245,0)');
   });
   test('unknownToRgba correctly converts any valid color string to RGBA', () => {
     expect(unknownToRgba('#000000')).toEqual('rgba(0,0,0,0)');
@@ -30,8 +45,32 @@ describe('Conversion module', () => {
     expect(unknownToRgba('rgb(0,0,0)')).toEqual('rgba(0,0,0,0)');
     expect(unknownToRgba('rgb(255,255,255)')).toEqual('rgba(255,255,255,0)');
     expect(unknownToRgba('rgba(255,255,255,0)')).toEqual('rgba(255,255,255,0)');
+    expect(unknownToRgba('rgba(0,0,0,0)')).toEqual('rgba(0,0,0,0)');
   });
   test('rgbaToArray correctly converts RGBA to an array of values', () => {
-    expect(rgbaToArray('rgba(0,0,0,0)')).toEqual(['0','0','0','0']);
+    expect(rgbaToArray('rgba(0,0,0,0)')).toEqual(['0', '0', '0', '0']);
+    expect(rgbaToArray('rgba(255,255,255,1)')).toEqual(['255', '255', '255', '1']);
+    expect(rgbaToArray('rgba(1,23,234,1)')).toEqual(['1', '23', '234', '1']);
+    expect(rgbaToArray('rgba(34,5,21,0)')).toEqual(['34', '5', '21', '0']);
+  });
+});
+
+describe('Conversion module (failing ❌)', () => {
+  xtest('hexToDec throws error when given invalid hex string', () => {});
+  test('hexToRgba throws error when given invalid hex string', () => {
+    expect(hexToRgba('abc')).toThrowError(Error);
+  });
+  xtest('decToHex throws error when given invalid number', () => {});
+  test('rgbToHex throws error when given invalid rgb string', () => {
+    expect(rgbToHex('abc')).toThrowError();
+  });
+  test('rgbToRgba throws error when given invalid rgb string', () => {
+    expect(rgbToRgba('abc')).toThrowError();
+  });
+  test('unknownToRgba throws error when given invalid color string', () => {
+    expect(unknownToRgba('abc')).toThrowError();
+  });
+  test('rgbaToArray throws error when given invalid RGBA string', () => {
+    expect(rgbaToArray('abc')).toThrowError();
   });
 });
